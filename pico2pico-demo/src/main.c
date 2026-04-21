2428b6d3f808851546b037faabd13938f8c72be3
Casper <casper.bjorkman@gmail.com>
2026-04-21 16:09:03 +0200

    Usb works

--- pico2pico-demo/src/main.c
+++ pico2pico-demo/src/main.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
-#include <zephyr/drivers/i2c.h>
 
-int main() {
+#include <zephyr/drivers/uart.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+             "Console device is not ACM CDC UART device");
+
+int main(void) {
+    const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+    uint32_t dtr = 0;
+
+    /* Poll if the DTR flag was set */
+    while (!dtr) {
+        uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+        /* Give CPU resources to low priority threads. */
+        k_sleep(K_MSEC(100));
+    }
+
     while (1) {
+        printk("Hello World! %s\n", CONFIG_ARCH);
+        k_sleep(K_SECONDS(1));
     }
 }
