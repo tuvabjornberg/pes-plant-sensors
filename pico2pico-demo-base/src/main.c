fbb4b9822f0d72ca7a7f400a92ae71d4bb57cd31
Casper <casper.bjorkman@gmail.com>
2026-05-05 14:12:13 +0200

    Refactor out the interface

--- pico2pico-demo-base/src/main.c
+++ pico2pico-demo-base/src/main.c
@@ -5,21 +5,27 @@
 
 #define ADDR 0x60
 
-static const struct device *bus = DEVICE_DT_GET(DT_NODELABEL(i2c0));
-#define BUF_LEN 6
+static const struct device *bus = DEVICE_DT_GET(DT_NODELABEL(i2c1));
 int main(void) {
 
-    char buf[BUF_LEN] = "HELLO";
+    printk("STARTED \n");
     while (!device_is_ready(bus)) {
         printk("WAITING\n");
         k_msleep(200);
     }
     k_msleep(5000);
+    printk("STARTED 2j\n");
 
-    for (size_t i = 0; i < 256; i++) {
-        int sc = i2c_write(bus, buf, BUF_LEN, ADDR);
-        int sc2 = i2c_read(bus, buf, BUF_LEN, ADDR);
-        printk("AjSD %d %d %d\n", i, sc, sc2);
+    for (size_t i = 0; i < 10; i++) {
+        uint8_t out = 0;
+        int sc = i2c_reg_read_byte(bus, ADDR, (uint8_t)i, &out);
+        printk("Read %d %d out:%d\n", i, sc, out);
+        k_msleep(500);
+    }
+
+    for (size_t i = 0; i < 20; i++) {
+        int sc = i2c_reg_write_byte(bus, ADDR, (uint8_t)i, 100 + i);
+        printk("Wrote %d %d \n", i, sc);
         k_msleep(500);
     }
 
--- pico2pico-demo/src/main.c
+++ pico2pico-demo/src/main.c
@@ -1,89 +1,37 @@
-
+#include "handler_interface.h"
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-static const struct device *bus = DEVICE_DT_GET(DT_NODELABEL(i2c0));
-static char last_byte;
-
-/*
- * @brief Callback which is called when a write request is received from the master.
- * @param config Pointer to the target configuration.
- */
-int sample_target_write_requested_cb(struct i2c_target_config *config) {
-    printk("sample target write requested\n");
+static const struct device *bus = DEVICE_DT_GET(DT_NODELABEL(i2c1));
+int handle_read(uint8_t reg, uint8_t *out) {
+    printk("[read] From 0x%02x\n", reg);
+    *out = reg;
     return 0;
 }
 
-/*
- * @brief Callback which is called when a write is received from the master.
- * @param config Pointer to the target configuration.
- * @param val The byte received from the master.
- */
-int sample_target_write_received_cb(struct i2c_target_config *config, uint8_t val) {
-    printk("sample target write received: 0x%02x\n", val);
-    last_byte = val;
+int handle_write(uint8_t reg, uint8_t val) {
+    printk("[write] To 0x%02x with 0x%02x\n", reg, val);
     return 0;
 }
-
-/*
- * @brief Callback which is called when a read request is received from the master.
- * @param config Pointer to the target configuration.
- * @param val Pointer to the byte to be sent to the master.
- */
-int sample_target_read_requested_cb(struct i2c_target_config *config, uint8_t *val) {
-    printk("sample target read request: 0x%02x\n", *val);
-    *val = 0x42;
-    return 0;
-}
-
-/*
- * @brief Callback which is called when a read is processed from the master.
- * @param config Pointer to the target configuration.
- * @param val Pointer to the next byte to be sent to the master.
- */
-int sample_target_read_processed_cb(struct i2c_target_config *config, uint8_t *val) {
-    printk("sample target read processed: 0x%02x\n", *val);
-    *val = 0x43;
-    return 0;
-}
-
-/*
- * @brief Callback which is called when the master sends a stop condition.
- * @param config Pointer to the target configuration.
- */
-int sample_target_stop_cb(struct i2c_target_config *config) {
-    printk("sample target stop callback\n");
-    return 0;
-}
-
-static struct i2c_target_callbacks sample_target_callbacks = {
-    .write_requested = sample_target_write_requested_cb,
-    .write_received = sample_target_write_received_cb,
-    .read_requested = sample_target_read_requested_cb,
-    .read_processed = sample_target_read_processed_cb,
-    .stop = sample_target_stop_cb,
-};
 
 int main(void) {
 
-    struct i2c_target_config cfg = {
-        .address = 0x60,
-        .callbacks = &sample_target_callbacks,
-    };
     printk("Target start\n");
-
-    // while (!device_is_ready(bus)) {
-    //     printk("I2C not ready\n");
-    // }
+    handler_config_t cfg = (handler_config_t){
+        .handle_read = handle_read,
+        .handle_write = handle_write,
+        .address = 0x60,
+    };
     int ret;
-    while ((ret = i2c_target_register(bus, &cfg)) != 0) {
-
+    if ((ret = register_target_with_handlers(bus, &cfg)) != 0) {
         printk("register: %d\n", ret);
+        while (true) {
+            printk("ERR: %d\n", ret);
+        }
     }
 
     while (1) {
-        printk("Hello\n");
-        k_msleep(125);
+        k_msleep(1000);
     }
 }
