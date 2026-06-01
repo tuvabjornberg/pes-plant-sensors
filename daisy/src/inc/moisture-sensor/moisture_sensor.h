bed268a17aae8fc88b93f9843a4e70edbe2a152d
Casper <casper.bjorkman@gmail.com>
2026-06-01 13:55:11 +0200

    Remove unnessasary prototype

--- daisy/src/inc/moisture-sensor/moisture_sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOISTURE_SENSOR_H
-#define MOISTURE_SENSOR_H
-
-#include <stdint.h>
-
-int init_moisture_sensor(const struct device *i2c_bus);
-int read_moisture_sensor(uint16_t *moisture);
-
-#endif
